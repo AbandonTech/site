3ef1912656972ddece4a910dcbc0dfae2461cc75
GDWR <gregory.dwr@gmail.com>
2023-02-25 18:37:54 +0000

    Enable hotreloading on Windows

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
     reactStrictMode: true,
-    output: 'standalone'
+    output: 'standalone',
+    // Enables hotreloading on Windows
+    webpackDevMiddleware: config => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        }
+        return config
+    },
 }
