cf5da9f5dcc264487cdb55651e60a3bbbe2609cb
fisher60 <wakefollower@gmail.com>
2023-05-15 13:19:54 -0500

    remove buggy split display
    While I had fun making it, I could never get the split display to work as intended. Even the best-case-scenario version would not be very accessible and would make it difficult to read. I am removing it now, but leaving it in the codebase just in case we wish to use it someday.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,12 @@
 import AboutCards from "@/components/aboutCards";
 import Banner from "@/components/banner";
 import Navbar from "@/components/navbar";
-import SplitDisplay from "@/components/splitDisplay";
 export default function Home() {
     return (
         <>
             <Navbar />
             <Banner />
             <AboutCards />
-            <SplitDisplay />
         </>
     )
 }
