5d0ab542d862bf61d359e2ec7310d759de2dc4ed
fisher60 <wakefollower@gmail.com>
2023-05-15 14:18:12 -0500

    add mobile responsiveness to about cards
    I changed about cards to default to flex-col, they will be flex-row with spacing on medium+ sized screens

--- src/components/aboutCards.tsx
+++ src/components/aboutCards.tsx
@@ -9,21 +9,26 @@ const hostingDesc = <ul className="list-disc pl-6">
 const webDev = <ul className="list-disc pl-6">
         <li>Design</li>
         <li>Website Mangement</li>
-        <li>Deployment</li>
+        <li>API Integrations</li>
     </ul>
 
 const anything = <ul className="list-disc pl-6">
         <li>Social Media Management</li>
         <li>Custom PC/Server Builds</li>
-        <li>Don&apos;t see what you need? <a className="underline hover:cursor-pointer" href="#contactUs">Ask</a></li>
+        <li>3D Printing</li>
     </ul>
 
 export default function AboutCards() {
     return (
-        <div className="flex flex-row items-center justify-center space-x-24 mt-12">
-            <Card title="Hosting" description={hostingDesc} />
-            <Card title="Web Dev" description={webDev} />
-            <Card title="Anything Else" description={anything} />
+        <div className="flex flex-col items-center justify-center mt-12">
+            <div className="flex flex-col md:flex-row md:space-x-24">
+                <Card title="Hosting" description={hostingDesc} />
+                <Card title="Web Dev" description={webDev} />
+                <Card title="Anything Else" description={anything} />
+            </div>
+            <div className="text-4xl mt-12">
+                <p>Don&apos;t see what you need? <a className="underline hover:cursor-pointer" href="#contactUs">Ask us</a></p>
+            </div>
         </div>
     )
 }
