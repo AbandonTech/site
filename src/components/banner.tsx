6ad7888e099f292f1b1e71ed60cbcc5befa2a869
fisher60 <wakefollower@gmail.com>
2023-05-15 14:47:14 -0500

    add mobile responsiveness for entire site
    I have finished adding the small tweaks needed to make the site mobile responsive.

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -2,11 +2,11 @@ export default function Banner() {
     return (
         <div className="w-full h-[50vh] bg-black">
             <div className="flex flex-row h-full items-center justify-center">
-                <div className="text-white mr-10">
-                    <h1>Logo</h1>
+                <div className="text-white pr-10">
+                    <h1 className="select-none text-8xl md:text-9xl">@</h1>
                 </div>
                 <div className="h-full border-solid border-white border-8 p-2"></div>
-                <div className="text-white text-7xl ml-10">
+                <div className="text-white text-5xl md:text-7xl ml-10">
                     <h1>Abandon</h1>
                     <h1>Tech</h1>
                 </div>
