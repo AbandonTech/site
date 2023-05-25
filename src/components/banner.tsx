ae75aa2c42c72633a90b716c96a497acf9620d10
Kyler Roloff <wakefollower@gmail.com>
2023-05-25 14:40:51 -0500

    adjust banner text size mobile, add strong style

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -6,16 +6,16 @@ import bgImage from "@/components/curveBgClipped.svg";
 export default function Banner() {
     return (
         <div>
-            <div className="text-gray-950 bg-white py-20">
+            <div className="text-gray-950 bg-white py-20 md:py-32">
                 <div className="flex flex-col items-center">
                     <h1 className="text-5xl md:text-9xl font-primary font-bold">AbandonTech</h1>
-                    <h3 className="text-xl md:text-4xl mt-6"><span className="text-orange-400">Custom</span> solutions to fit your <span className="text-orange-400">business</span></h3>
+                    <h3 className="text-base md:text-4xl mt-6"><strong className="text-orange-400">Custom</strong> solutions to fit your <strong className="text-orange-400">business</strong></h3>
 
-                    <a className="bg-white bg-opacity-0 mt-10 hover:bg-opacity-5 text-orange-400 font-semibold hover:text-orange-200 py-2 px-4 border border-orange-400 hover:border-orange-200 hover:cursor-pointer rounded" href="#contactUs">Contact Us</a>
+                    <a className="bg-white text-xl bg-opacity-0 mt-16 hover:bg-opacity-5 text-orange-400 font-semibold py-4 px-6 border border-orange-400 hover:border-orange-600 hover:text-orange-600 hover:bg-orange-300 hover:cursor-pointer rounded" href="#contactUs">Contact Us</a>
                 </div>
             </div>
 
-            <img src={bgImage.src} className="w-screen" alt="Orange, curved background for website banner"/>
+            <img src={bgImage.src} className="w-full" alt="Orange, curved background for website banner"/>
             
         </div>
     )
