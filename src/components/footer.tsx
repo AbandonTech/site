b737ded273211924ef5807cc88b61af078b7d7db
Kyler Roloff <wakefollower@gmail.com>
2023-05-26 22:03:59 -0500

    change email to use info@at, add github link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
-import envelopImage from "@/components/envelope.svg";
+import mailIcon from "@/components/envelope.svg";
+import githubIcon from "@/components/github.svg";
 
+const infoEmail = "Info@AbandonTech.cloud"
 export default function Footer() {
     return (
-        <div id="contactUs" className="flex justify-center md:justify-start w-screen text-orange-500 bg-white p-16">
-            <div className="flex-col">
-                <a  href="mailto:abandontech@gmail.com"><Image className="inline-block mr-1" src={envelopImage} width={20} height={50} alt="envelop icon" />AbandonTech@gmail.com</a>
+        <div id="contactUs" className="flex justify-center md:justify-start w-screen text-lg text-orange-500 bg-white p-16">
+            <div className="flex flex-col space-y-2 text-center md:text-left">
+                <a  href={`mailto:${infoEmail}`}><Image className="inline-block mr-1" src={mailIcon} width={20} height={50} alt="email envelop icon" />{infoEmail}</a>
+                <a  href="https://github.com/AbandonTech" target="_blank" rel="noreferrer"><Image className="inline-block mr-1" src={githubIcon} width={20} height={50} alt="github icon" />Github</a>
             </div>
         </div>
     )
