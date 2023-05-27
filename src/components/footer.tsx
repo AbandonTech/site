8f042f74f39d34ccf508982476c5d3c5f27b8aa2
Kyler Roloff <wakefollower@gmail.com>
2023-05-26 20:05:31 -0500

    fix website overflowing when using contactus anchor tag

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,11 +5,11 @@ import envelopImage from "@/components/envelope.svg";
 
 export default function Footer() {
     return (
-        <div id="contactUs" className="relative w-screen">
+        <div className="relative w-screen">
             <img src={bgImage.src} className="md:hidden w-full rotate-180 z-0 select-none" alt="Orange, curved background for website banner"/>
 
-            <div className="text-gray-950 w-full bg-white p-6 md:p-0 md:bg-transparent md:h-48 md:z-10 md:absolute md:top-14">
-                <div className="flex flex-row justify-center w-full md:w-2/3 md:top-64">
+            <div className="text-gray-950 w-full bg-white p-6 md:p-0 md:bg-transparent md:z-10 md:absolute md:top-14">
+                <div id="contactUs" className="flex flex-row justify-center w-full md:w-2/3 md:top-64">
                     <span><a href="mailto:abandontech@gmail.com"><Image className="inline-block mr-1" src={envelopImage} width={20} height={50} alt="envelop icon" />AbandonTech@gmail.com</a></span>
                 </div>
             </div>
